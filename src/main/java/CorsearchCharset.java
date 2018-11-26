76ebc3dcd800d3b9687d35c403d59b84d5892a79
jpcuve <gexcuj01i>
2018-11-26 10:23:41 +0100

    .

--- src/main/java/CorsearchCharset.java
+++ src/main/java/CorsearchCharset.java
@@ -1,4 +1,5 @@
 import java.nio.charset.StandardCharsets;
+import java.text.Collator;
 
 public class CorsearchCharset {
     public static byte[] NAME_1 = {0x50,0x49,0x2D,0x44,0x65,0x73,0x69,0x67,0x6E};
@@ -10,6 +11,12 @@ public class CorsearchCharset {
         var nameTwo = new String(NAME_2, StandardCharsets.UTF_8);
         System.out.printf("|%s|%n", nameOne);
         System.out.printf("|%s|%n", nameTwo);
+        Collator collator = Collator.getInstance();
+        System.out.println(collator.getCollationKey(nameOne));
+        int result = collator.compare(nameOne, nameTwo);
+        System.out.printf("%s%n", result);
+        result = nameOne.compareToIgnoreCase(nameTwo);
+        System.out.printf("%s%n", result);
 
     }
 }
