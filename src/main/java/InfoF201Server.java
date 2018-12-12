ede6c250b3e41f3cc6368b5c87ddae3f576b9406
jpcuve <gexcuj01i>
2018-12-12 14:05:02 +0100

    .

--- src/main/java/InfoF201Server.java
+++ src/main/java/InfoF201Server.java
@@ -6,9 +6,9 @@ public class InfoF201Server {
     public static void main(String[] args) throws IOException  {
         try(ServerSocket serverSocket = new ServerSocket(5555)){
             while(true){
-                Socket socket = serverSocket.accept();
+                Socket socket = serverSocket.accept(); // bloquant, jusqu'à ce qu'une connection avec un client est établie
                 socket.setSoTimeout(10_000); // temps maximum d'attente du caractère suivant sur le socket
-                new Thread(new InfoF201Worker(socket)).start();
+                new Thread(new InfoF201Worker(socket)).start(); // processing du client lancé sur thread séparé
             }
         }
     }
--- src/main/java/InfoF201Worker.java
+++ src/main/java/InfoF201Worker.java
@@ -30,22 +30,31 @@ public class InfoF201Worker implements Runnable {
 
     @Override
     public void run() {
+        // on démarre de l'état zéro, correspondant à la lecture de l'année.
         int state = 0;
         reset();
         try (socket) {
-            while (state < 5) {
-                int read = socket.getInputStream().read();
+            while (state < 5) { // l'état 5 correspond à l'état de sortie du worker
+                int read = socket.getInputStream().read(); // lire le caractère (en java le byte est délivré comme int
+                // valeur 0 à 255, -1 correspond à end-of-stream). Cette methode est bloquante, le timeout a été
+                // défini comme paramètre du serveur
                 switch (state) {
-                    case 0:
+                    case 0: // lecture de l'année
                         if (read != STOP_BYTE) {
+                            // tant qu'on est pas arrivé au stop byte, on accumule les caractères lu dans un buffer
                             buffer[ptr++] = (byte) read;
+                            // avec ici la limite de 4 caractères max pour l'année
                             checkSize(4);
                         } else {
+                            // ici, on a fini d'accumuler les caractères de l'année
+                            // on convertit l'année en int et on la stocke dans une variable locale au worker
+                            // (en C ce sera peut être une variable locale au thread du worker)
                             year = Integer.parseInt(reset());
+                            // et on passe à l'état 1, lecture du  mois
                             state = 1;
                         }
                         break;
-                    case 1:
+                    case 1: // lecture du mois, etc.
                         if (read != STOP_BYTE) {
                             buffer[ptr++] = (byte) read;
                             checkSize(2);
